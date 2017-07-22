59eca7ebebb1b2a9d3a90a54c8d081122b3b3ae4
Thamodaran <kthamodaran@gmail.com>
2017-07-23 01:17:09 +0530

    First adminLTE base setup

---
+++ vendorCopy/laravel/framework/src/Illuminate/Console/AppNamespaceDetectorTrait.php
@@ -0,0 +1,15 @@
+<?php
+namespace Illuminate\Console;
+use Illuminate\Container\Container;
+trait AppNamespaceDetectorTrait
+{
+    /**
+     * Get the application namespace.
+     *
+     * @return string
+     */
+    protected function getAppNamespace()
+    {
+        return Container::getInstance()->getNamespace();
+    }
+}
